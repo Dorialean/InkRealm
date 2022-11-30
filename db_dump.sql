0ac49c0531cad662e6d8fc5db76317882c7c20c4
Dorialean <lana.lakutina@mail.ru>
2022-11-30 18:14:39 +0300

    working dump

--- db_dump.sql
+++ db_dump.sql
@@ -16,29 +16,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE IF EXISTS ink_realm;
---
--- Name: ink_realm; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE ink_realm WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'Russian_Russia.1251';
-
-
-ALTER DATABASE ink_realm OWNER TO postgres;
-
-\connect ink_realm
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 --
 -- Name: profession; Type: TYPE; Schema: public; Owner: postgres
 --
