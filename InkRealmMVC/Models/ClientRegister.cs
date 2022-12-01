099654331f9be14edf4087ef17659104a13b2130
Dorialean <lana.lakutina@mail.ru>
2022-12-01 18:28:04 +0300

    client reg ui

---
+++ InkRealmMVC/Models/ClientRegister.cs
@@ -0,0 +1,15 @@
+﻿namespace InkRealmMVC.Models
+{
+    public class ClientRegister
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string? FatherName { get; set; }
+        public string? MobilePhone { get; set; }
+        public string? Email { get; set; }
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime Registered { get; set; }
+    }
+}
