dd32d0d8bef96e25b01bca375937a8516f9ca1ab
Dorialean <lana.lakutina@mail.ru>
2022-12-21 17:45:01 +0300

    Client Space Refactor

--- InkRealmMVC/Controllers/ClientController.cs
+++ InkRealmMVC/Controllers/ClientController.cs
@@ -2,10 +2,12 @@
 using InkRealmMVC.Models.DbModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Net;
 
 namespace InkRealmMVC.Controllers
 {
+    [Authorize(Roles = Role.InkClient)]
     public class ClientController : Controller
     {
         private readonly InkRealmContext _context;
@@ -15,33 +17,42 @@ namespace InkRealmMVC.Controllers
             _context = context; 
         }
 
-        [Authorize(Roles = Role.InkClient)]
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             using (_context)
             {
-                InkClient inkClient = _context.InkClients.First(c => c.Login == User.Identity.Name);
-                List<Order> orders = _context.Orders.Select(o => o).Where(o => o.ClientId == inkClient.ClientId).ToList();
-                List<InkService> clientServices = new();
-                List<InkMaster> mastersComeTo = new();
-                List<MasterReviews> reviews = _context.MasterReviews.Select(r => r).Where(r => r.ClientId == inkClient.ClientId).ToList();
-                List<InkProduct> orederedProducts = (from prod in _context.InkProducts.ToList() join ord in _context.Orders.ToList()
+                InkClient inkClient = await _context.InkClients.FirstAsync(c => c.Login == User.Identity.Name);
+                List<Order> orders = await _context.Orders.Select(o => o).Where(o => o.ClientId == inkClient.ClientId).ToListAsync();
+                List<InkClientService> clientServices = await _context.InkClientServices.Select(s => s).Where(s => s.ClientId == inkClient.ClientId).ToListAsync();
+                List<InkService> neededServices = (from cServ in clientServices join aServ in await _context.InkServices.ToListAsync()
+                                                   on cServ.ServiceId equals aServ.ServiceId
+                                                   where cServ.ClientId == inkClient.ClientId
+                                                   select aServ).ToList();
+                List<InkMaster> mastersComeTo = (from cServ in clientServices join mast in await _context.InkMasters.ToListAsync()
+                                                 on cServ.MasterId equals mast.MasterId
+                                                 where cServ.ClientId == inkClient.ClientId
+                                                 select mast).ToList();
+
+                List<MasterReviews> reviews = await _context.MasterReviews.Select(r => r).Where(r => r.ClientId == inkClient.ClientId).ToListAsync();
+                List<InkProduct> orederedProducts = (from prod in await _context.InkProducts.ToListAsync() join ord in await _context.Orders.ToListAsync()
                                                       on prod.ProductId equals ord.ProductId
                                                      where ord.ClientId == inkClient.ClientId
                                                      select prod).ToList();
                                                   
 
-                return View(new ClientSpaceModel()
+                return await Task.Run(() =>  View(new ClientSpaceModel()
                 {
                     Client = inkClient,
-                    Orders = orders,
-                    ClientServices = clientServices,
+                    NeededServices = neededServices,
                     MastersToCome = mastersComeTo,
+                    ClientServices = clientServices,
                     ClientReviewd = reviews,
                     OrderedProducts = orederedProducts
-                });
+                }));
             }
         }
+
+        
     }
 }
--- InkRealmMVC/Models/ClientSpaceModel.cs
+++ InkRealmMVC/Models/ClientSpaceModel.cs
@@ -5,11 +5,10 @@ namespace InkRealmMVC.Models
     public class ClientSpaceModel
     {
         public InkClient Client { get; set; }
-        public List<Order>? Orders { get; set; }
-        public List<InkService>? ClientServices { get; set; }
+        public List<InkService>? NeededServices { get; set; }
         public List<InkMaster>? MastersToCome { get; set; }
         public List<MasterReviews>? ClientReviewd { get; set; }
-        public List<InkProduct>? WantedProducts { get; set; }
+        public List<InkClientService>? ClientServices { get; set; }
         public List<InkProduct>? OrderedProducts { get; set; }
 
     }
